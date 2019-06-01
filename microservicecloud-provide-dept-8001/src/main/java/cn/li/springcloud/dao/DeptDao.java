cc425794744cdead2a453f0160ae3a60546d63e7
Dale_li <li.747166@gmail.com>
2019-06-01 17:36:57 +0800

    微服务

---
+++ microservicecloud-provide-dept-8001/src/main/java/cn/li/springcloud/dao/DeptDao.java
@@ -0,0 +1,17 @@
+package cn.li.springcloud.dao;
+
+import cn.li.entities.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DeptDao {
+
+    public boolean addDept(Dept dept);
+
+    public Dept findById(Long id);
+
+    public List<Dept> findAll();
+
+}
