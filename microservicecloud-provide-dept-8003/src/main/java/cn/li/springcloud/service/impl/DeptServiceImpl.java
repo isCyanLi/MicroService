1abb0160784a29d6be129600eb91900b1746f283
Dale_li <li.747166@gmail.com>
2019-06-03 21:19:09 +0800

    Ribbon负载均衡

---
+++ microservicecloud-provide-dept-8003/src/main/java/cn/li/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,32 @@
+package cn.li.springcloud.service.impl;
+
+import cn.li.entities.Dept;
+
+import cn.li.springcloud.dao.DeptDao;
+import cn.li.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+    @Autowired
+    private DeptDao dao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return dao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return dao.findAll();
+    }
+}
